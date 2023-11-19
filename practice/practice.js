21031ad50a745e7e0a250603e3209540b07ae51f
Narwhal Kid <madgamer2525@gmail.com>
2023-11-19 11:10:33 -0800

    Update practice.js

--- practice/practice.js
+++ practice/practice.js
@@ -1,10 +1,7 @@
 // Set var code to url code
 const urlParams = new URLSearchParams(window.location.search);
 const code = urlParams.get('code'); 
-const deck = urlParams.get('deck'); 
 console.log(code)
-
-// if(deck === null) window.location.replace(window.location + "&code=1111222233334444")
 
 // Alert user if code is not correct length
 if(code.length !== 16) alert('Code is not 16 characters long. Please try again.')
